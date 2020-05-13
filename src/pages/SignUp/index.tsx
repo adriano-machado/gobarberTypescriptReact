6388cbc8739ed5fcabdf4137db3a726e8244d90f
Adriano Machado <adrianoricardom@gmail.com>
2020-05-13 19:16:00 -0300

    add: validation erros using yup and unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
-
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-import logoImg from '../../assets/logo.svg';
+import * as Yup from 'yup';
+
 import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationError from '../../utils/getValidationErros';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {}
+  const formRef = useRef<FormHandles>(null);
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name is mandatory'),
+        email: Yup.string()
+          .required('E-mail is mandatory')
+          .email('You must enter a valid e-mail'),
+        password: Yup.string().min(6, 'At least 6 digits'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationError(err);
+      formRef.current?.setErrors(errors);
+
+      console.log(err);
+    }
+  }, []);
   return (
     <Container>
       <Background />
 
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <Form initialData={{}} onSubmit={handleSubmit}>
+        <Form ref={formRef} initialData={{}} onSubmit={handleSubmit}>
           <h1>Sign up</h1>
           <Input name="name" icon={FiUser} placeholder="Name" />
 
