13d7f3a92ccc0010e723e9b4ca8320bf1363661c
Adriano Machado <adrianoricardom@gmail.com>
2020-05-13 18:51:59 -0300

    add: focus and filled properties

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,54 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 import { Container } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon: React.ComponentType<IconBaseProps>;
 }
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input {...rest} type="text" />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocus] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocus(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocus(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  return (
+    <Container isFocused={isFocused} isFilled={isFilled}>
+      {Icon && <Icon />}
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+        type="text"
+      />
+    </Container>
+  );
+};
 
 export default Input;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,37 +1,42 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+
+import { Form } from '@unform/web';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {}
+  return (
+    <Container>
+      <Background />
 
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <form>
-        <h1>Sign up</h1>
-        <Input name="name" icon={FiUser} placeholder="Name" />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form initialData={{}} onSubmit={handleSubmit}>
+          <h1>Sign up</h1>
+          <Input name="name" icon={FiUser} placeholder="Name" />
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
-        <Button type="submit">Register</Button>
-      </form>
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+          <Button type="submit">Register</Button>
+        </Form>
 
-      <a href="forgot">
-        <FiArrowLeft size={20} />
-        Back to logon
-      </a>
-    </Content>
-  </Container>
-);
+        <a href="forgot">
+          <FiArrowLeft size={20} />
+          Back to logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
