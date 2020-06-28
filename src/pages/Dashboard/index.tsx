8eded83bab8be0ff8e05fd82375c8b461a5cc815
Adriano Machado <adrianoricardom@gmail.com>
2020-06-28 17:44:10 -0300

    add nex appointment to dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../context/AuthContext';
 
@@ -30,6 +39,32 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Schedules</h1>
+          <p>
+            <span>Today</span>
+            <span>Day 06</span>
+            <span>SMonday</span>
+          </p>
+          <NextAppointment>
+            <strong>Next schedule</strong>
+            <div>
+              <img
+                src="https://api.adorable.io/avatars/285/abott@adorable.png"
+                alt="teste"
+              />
+              <strong>Adriano Machado</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
